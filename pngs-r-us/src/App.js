131d4c661cd2dfe41e81823b6615650108e6f2f3
tzits <tzitsman@gmail.com>
2022-11-17 13:19:05 -0500

    final routing set up

--- pngs-r-us/src/App.js
+++ pngs-r-us/src/App.js
@@ -3,11 +3,18 @@ import Nav from './routes/nav/nav.component'
 import Home from './routes/home/home.component'
 
 
+const Shop = () => {
+  return(
+    <h1>I am the Shop Page</h1>
+  )
+}
+
 const App = () => {
   return (
     <Routes>
       <Route path= '/' element={<Nav />}>
         <Route index element={<Home />} />
+        <Route path= '/shop' element={<Shop />} />
       </Route>
     </Routes>
   )
--- pngs-r-us/src/routes/nav/nav.component.jsx
+++ pngs-r-us/src/routes/nav/nav.component.jsx
@@ -1,12 +1,14 @@
 import { Fragment } from 'react'
 import { Outlet, Link } from 'react-router-dom'
+import { ReactComponent as CrwnLogo } from '../../assets/crown.svg'
+import './nav.styles.scss'
 
 const Nav = () => {
     return (
         <Fragment>
             <div className='nav'>
                 <Link className='logo-container' to='/'>
-                    <div>LOGO</div>
+                    <CrwnLogo className='logo' />
                 </Link>
                 <div className='nav-links-container'>
                     <Link classsName='nav-link' to='/shop'>
