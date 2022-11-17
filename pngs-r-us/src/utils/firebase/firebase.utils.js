275baf528a6341e6e993285d2e4f7a6d40a3ed96
tzits <tzitsman@gmail.com>
2022-11-17 15:31:51 -0500

    create doc in database

--- pngs-r-us/src/utils/firebase/firebase.utils.js
+++ pngs-r-us/src/utils/firebase/firebase.utils.js
@@ -38,4 +38,25 @@ export const db = getFirestore();
 export const createUserDocumentFromAuth = async(userAuth) => {
     const userDocRef = doc(db, 'users', userAuth.uid)
     console.log(userDocRef)
+
+    const userSnapshot = await getDoc(userDocRef)
+    console.log(userSnapshot)
+    console.log(userSnapshot.exists())
+
+    if(!userSnapshot.exists()) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await setDoc(userDocRef, {
+                displayName,
+                email,
+                createdAt
+            });
+        } catch (error) {
+            console.log('error creating user', error.message )
+        }
+    }
+
+    return userDocRef
 }
