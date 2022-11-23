609c2e1657e03d1d0b48c738e4b8638444d1f10e
tzits <tzitsman@gmail.com>
2022-11-23 14:22:55 -0500

    created shopping cart icon and toggle feauture using context

--- pngs-r-us/src/index.js
+++ pngs-r-us/src/index.js
@@ -5,6 +5,7 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App';
 import { UserProvider } from './contexts/user.context';
 import { ProductsProvider } from './contexts/products.context';
+import { CartProvider } from './contexts/cart.context';
 
 import './index.scss';
 
@@ -12,11 +13,13 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <ProductsProvider>
-        <UserProvider>
-          <App />
-        </UserProvider>
+      <UserProvider>
+        <ProductsProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
         </ProductsProvider>
+        </UserProvider>
     </BrowserRouter>
 
   </React.StrictMode>
