b3c4fd496a68fdb8080e45a610ab2a0aeecbb67f
tzits <tzitsman@gmail.com>
2022-11-24 22:25:48 -0500

    finally can remove items from dropdown

--- pngs-r-us/src/components/cart-item/cart-item.component.jsx
+++ pngs-r-us/src/components/cart-item/cart-item.component.jsx
@@ -1,11 +1,22 @@
 import './cart-item.styles.scss'
+import { useContext } from 'react'
+import { CartContext } from '../../contexts/cart.context'
+
 
 const CartItem = ({ cartItem }) => {
-    const { name, quantity } = cartItem
+    const { clearItemFromCart } = useContext(CartContext)
+
+    const clearItemHandler = () => clearItemFromCart(cartItem)
+
+    const { name, quantity, imageUrl, price } = cartItem
     return (
-        <div>
-            <h2>{name}</h2>
-            <span>{quantity}</span>
+        <div className='cart-item-container'>
+            <img src={imageUrl} alt={name} />
+            <div className='item-details'>
+                <span className='name'>{name}</span>
+                <span className='price'>{quantity} x ${price}</span>
+            </div>
+            <span className='remove-button' onClick={clearItemHandler}>&#10005;</span>
         </div>
     )
 }
