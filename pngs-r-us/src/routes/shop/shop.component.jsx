0796edd0f914fb2b2a27a8fa6b31efa2d946f1f7
tzits <tzitsman@gmail.com>
2022-11-25 13:04:42 -0500

    pull from firestore and change products to categories

--- pngs-r-us/src/routes/shop/shop.component.jsx
+++ pngs-r-us/src/routes/shop/shop.component.jsx
@@ -1,23 +1,26 @@
-import  { useContext } from 'react'
+import  { useContext, Fragment } from 'react'
 
-import { ProductsContext } from '../../contexts/products.context'
+import { CategoriesContext } from '../../contexts/categories.context'
 import ProductCard from '../../components/product-card/product-card.component'
 
 import './shops.style.scss'
 
 const Shop = () => {
-    const { products } = useContext(ProductsContext)
+    const { categoriesMap } = useContext(CategoriesContext)
 
     return (
-        <div className='products-container'>
-            {
-                products.map((product) => (
-                    <ProductCard key={product.id} product={product} />
-                )
-            
-            
+        <Fragment>
+            {Object.keys(categoriesMap).map(title => 
+                <Fragment key={title}>
+                    <h2>{title}</h2>
+                    <div className='products-container'>
+                        {categoriesMap[title].slice(0,4).map((product) => (
+                            <ProductCard key={product.id} product={product} />
+                        ))}
+                    </div>
+                </Fragment>
             )}
-        </div>
+        </Fragment>
     )
 }
 
