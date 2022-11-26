2ad64144251104dfc5b3c9251527032287233888
tzits <tzitsman@gmail.com>
2022-11-26 06:18:31 -0500

    cart reducer

--- pngs-r-us/src/contexts/user.context.jsx
+++ pngs-r-us/src/contexts/user.context.jsx
@@ -1,5 +1,7 @@
 import { createContext, useEffect, useReducer } from "react";
 import { onAuthStateChangedListener, createUserDocumentFromAuth } from '../utils/firebase/firebase.utils'
+import { createAction } from "../utils/firebase/reducer/reducer.utils";
+
 
 // as actual value to access
 export const UserContext = createContext({
@@ -35,7 +37,9 @@ export const UserProvider = ( {children} ) => {
     const [ { currentUser }, dispatch ] = useReducer(userReducer, INITIAL_STATE);
 
     const setCurrentUser = (user) => {
-        dispatch({type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user})
+        dispatch(
+            createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user)
+        )
     }
 
     const value = { currentUser, setCurrentUser }
