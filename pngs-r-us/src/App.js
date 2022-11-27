6bf5305b90e457c705832f6e92cb3cb69fe1a4a0
tzits <tzitsman@gmail.com>
2022-11-27 00:00:49 -0500

    set up user redux

--- pngs-r-us/src/App.js
+++ pngs-r-us/src/App.js
@@ -1,11 +1,29 @@
 import { Routes, Route } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
+import { useEffect } from 'react'
 import Nav from './routes/nav/nav.component'
 import Home from './routes/home/home.component'
 import Authentication from './routes/authentication/authentication.component'
 import Shop from './routes/shop/shop.component'
 import Checkout from './routes/checkout/checkout.component'
+import { onAuthStateChangedListener, createUserDocumentFromAuth } from './utils/firebase/firebase.utils'
+import { setCurrentUser } from './store/user/user.action'
+
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChangedListener((user) => {
+        if (user) {
+            createUserDocumentFromAuth(user)
+        }
+        dispatch(setCurrentUser(user));
+    })
+
+    return unsubscribe
+  },[])
+
   return (
     <Routes>
       <Route path= '/' element={<Nav />}>
--- pngs-r-us/src/index.js
+++ pngs-r-us/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { store } from './store/store';
 
 import App from './App';
 import { UserProvider } from './contexts/user.context';
@@ -12,16 +14,15 @@ import './index.scss';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <UserProvider>
+    <Provider store={store}>
+      <BrowserRouter>
         <CategoriesProvider>
           <CartProvider>
             <App />
           </CartProvider>
         </CategoriesProvider>
-        </UserProvider>
-    </BrowserRouter>
-
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
 
