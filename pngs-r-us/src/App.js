b27219b9cfba3907678ef586372656ef4301dfdd
tzits <tzitsman@gmail.com>
2022-11-28 14:17:10 -0500

    user reducer

--- pngs-r-us/src/App.js
+++ pngs-r-us/src/App.js
@@ -1,4 +1,13 @@
 import { Routes, Route } from 'react-router-dom'
+import { useEffect } from 'react'
+import { 
+  onAuthStateChangedListener, 
+  createUserDocumentFromAuth 
+} from './utils/firebase/firebase.utils';
+import { useDispatch } from 'react-redux'
+
+import { setCurrentUser } from './store/user/user.action'
+
 import Nav from './routes/nav/nav.component'
 import Home from './routes/home/home.component'
 import Authentication from './routes/authentication/authentication.component'
@@ -6,6 +15,20 @@ import Shop from './routes/shop/shop.component'
 import Checkout from './routes/checkout/checkout.component'
 
 const App = () => {
+  const dispatch = useDispatch();
+
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChangedListener((user) => {
+        if (user) {
+            createUserDocumentFromAuth(user)
+        }
+        dispatch(setCurrentUser(user));
+    });
+
+    return unsubscribe
+},[])
+
   return (
     <Routes>
       <Route path= '/' element={<Nav />}>
