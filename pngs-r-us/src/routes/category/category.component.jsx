0190a57bcc1d6f0cdac1d65eb316dbc01317d2f0
tzits <tzitsman@gmail.com>
2022-11-28 08:11:50 -0500

    categories redux

--- pngs-r-us/src/routes/category/category.component.jsx
+++ pngs-r-us/src/routes/category/category.component.jsx
@@ -1,14 +1,17 @@
-import { useContext, useState, useEffect, Fragment } from 'react'
+import { useState, useEffect, Fragment } from 'react'
+import { useSelector } from 'react-redux'
 import { useParams } from 'react-router-dom'
 
-import { CategoriesContext } from '../../contexts/categories.context'
+import { selectCategoriesMap } from '../../store/category/category.selector'
+
 import ProductCard from '../../components/product-card/product-card.component'
 
 import { CategoryTitle, CategoryContainer} from './category.styles'
 
 const Category = () => {
+
     const { category } = useParams()
-    const { categoriesMap } = useContext(CategoriesContext);
+    const categoriesMap = useSelector(selectCategoriesMap);
     const [products, setProducts ] = useState(categoriesMap[category]);
 
     useEffect(() => {
--- pngs-r-us/src/routes/shop/shop.component.jsx
+++ pngs-r-us/src/routes/shop/shop.component.jsx
@@ -1,11 +1,25 @@
 import { Routes, Route } from 'react-router-dom'
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+
+import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils.js'
+import { setCategoriesMap } from '../../store/category/category.actions.js'
 
 import CategoriesPreview from '../categories-preview/categories-preview.component'
 import Category from '../category/category.component'
 
 const Shop = () => {
+    const dispatch = useDispatch()
 
+    useEffect(() => {
+        const getCategoriesMap = async () => {
+            const categoryMap = await getCategoriesAndDocuments('categories')
+            dispatch(setCategoriesMap(categoryMap));
+        }
+        getCategoriesMap()
+    }, [])
     return (
+
         <Routes>
             <Route index element={<CategoriesPreview />} />
             <Route path="/:category" element={<Category />} />
