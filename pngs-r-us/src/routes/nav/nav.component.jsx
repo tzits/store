a1a44d172438aef573582e0fed6be916bca583e8
tzits <tzitsman@gmail.com>
2022-11-28 14:28:39 -0500

    working user reducer

--- pngs-r-us/src/routes/nav/nav.component.jsx
+++ pngs-r-us/src/routes/nav/nav.component.jsx
@@ -1,17 +1,19 @@
 import { Fragment, useContext } from 'react'
-import { Outlet, Link } from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
 import CartIcon from '../../components/cart-icon/cart-icon.component'
 import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component'
+import { selectCurrentUser } from '../../store/user/user.selector'
+import { useSelector } from 'react-redux'
 
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg'
-import { UserContext } from '../../contexts/user.context'
 import { CartContext, CartProvider } from '../../contexts/cart.context'
 import { NavigationContainer, LogoContainer, NavLinks, NavLink } from './nav.styles'
 import { signOutUser } from '../../utils/firebase/firebase.utils'
 
 
 const Nav = () => {
-    const { currentUser } = useContext(UserContext)
+    const currentUser = useSelector(selectCurrentUser)
+    // const { currentUser } = useContext(UserContext)
     const { isCartOpen } = useContext(CartContext)
 
 
