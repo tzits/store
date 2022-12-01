5351d28f3d596233573d05d650d2ee79712e116f
tzits <tzitsman@gmail.com>
2022-12-01 14:18:29 -0500

    redux-persist added

--- pngs-r-us/src/store/store.js
+++ pngs-r-us/src/store/store.js
@@ -1,5 +1,7 @@
 import { compose, legacy_createStore as createStore, applyMiddleware } from 'redux'
-// import logger from 'redux-logger'
+
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'
 
 import { rootReducer } from './root-reducer'
 
@@ -16,8 +18,19 @@ const loggerMiddleware = (store) => (next) => (action) => {
     console.log('next state: ', store.getState())
 }
 
+const persistConfig = {
+    key: 'root',
+    storage,
+    blacklist: ['user']
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+
 const middleWares = [loggerMiddleware]
 
 const composedEnhancers = compose(applyMiddleware(...middleWares))
 
-export const store = createStore(rootReducer,undefined,composedEnhancers)
+export const store = createStore(persistedReducer,undefined,composedEnhancers)
+
+export const persistor = persistStore(store)
